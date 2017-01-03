016d8d5b2da7b77be21427605e2bc7e72744f8e8
sytnei <ionut_dinayahoo.comgit config --global user.email ionut_dinayahoo.com>
2017-01-03 11:59:23 +0200

    version 1.0.2 release

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # grunt-contrib-versioning2
-grunt assets versioning
+
+[Grunt][grunt] task to handle versioning of a project.
+
+## Getting Started
+_Requires grunt >=0.4.2. If you haven't used [grunt][] before, be sure to check out the [Getting Started][] guide._
+
+From the same directory as your project's [Gruntfile][Getting Started] and [package.json][], install this plugin by running the following command:
+
+```bash
+npm install grunt-contrib-versioning2 --save-dev
+```
+
+Once that's done, add this line to your project's Gruntfile:
+
+```js
+grunt.loadNpmTasks('grunt-contrib-versioning2');
+```
+
+## The "version" task
+
+### Overview
+In your project's Gruntfile, add a section named `versioning` to the data object passed into `grunt.initConfig()`.
+
+```js
+grunt.initConfig({
+  versioning: {
+        doc: 'index.html',
+        files: {
+        css: [],
+        js: [],
+        hastype : 'date'
+     }
+  }
+})
+```
---
+++ tasks/versioning.js
@@ -0,0 +1,83 @@
+'use strict';
+var fs = require('fs');
+var path = require('path');
+
+module.exports = function(grunt) {
+
+    var embeddedFiles = {};
+
+    grunt.registerMultiTask('versioning', 'Assets versioning', function() {
+        var filenames = getNames(this.data.files);
+        var doc = this.data.doc;
+        var hastype = (this.data.hastype) ? this.data.hastype : 'date';
+        var hashvalue = (this.data.hashvalue) ? this.data.hashvalue : Math.floor(Math.random() * 99999999);
+        var text = fs.readFileSync(doc, {
+            encoding: 'utf-8'
+        });
+
+
+        if (hastype == date){
+            var cd = new Date();
+            var codeVersion = cd.getFullYear() + cd.getMonth() + cd.getHours() + cd.getMinutes() + cd.getSeconds();
+        }
+        else if (hastype = 'custom'){
+            var codeVersion = hashvalue;
+        }
+
+        filenames.forEach(function (fileName) {
+            if (embeddedFiles[fileName]) {
+                text = injectFile(text, fileName);
+            } else {
+                text = getUpdatedText(text, fileName, codeVersion);
+            }
+        });
+        fs.writeFileSync(doc, text);
+        
+        for (var item in this.data.files) {
+            var name = this.data.files[item];
+            for (var i = 0, len = name.length; i < len; ++i) {
+                var fileName = name[i].src;
+                renameFile(fileName, codeVersion);
+            }
+        };
+    });
+    
+    function getUpdatedText(text, fileName, codeVersion) {
+        var parts = text.split(fileName);
+        var newtext = parts[0] + codeVersion + '.' + fileName + parts[1];
+        return newtext;
+    }
+
+    function injectFile (text, fileName) {
+        
+    }
+
+    function renameFile (name, codeVersion) {
+        var m = name.split('/');
+        var lastPart = m[m.length - 1];
+        var prefix = m.slice(0, m.length - 1).join('/');
+        var newName = prefix + '/' + codeVersion + '.' + lastPart;
+        fs.link(name, newName, function(err) {});
+    }
+
+    function getNames (files) {
+        var r = [];
+
+        for (var i = 0, len = files.css.length; i < len; ++i) {
+            var file = files.css[i];
+            var m = file.src.split('/');
+            r.push(m[m.length - 1]);
+            if (file.embedded) {
+                embeddedFiles[file.src] = file.src;
+            }
+        }
+
+        for (var i = 0, len = files.js.length; i < len; ++i) {
+            var file = files.js[i];
+            var m = file.src.split('/');
+            r.push(m[m.length - 1]);
+        }
+        return r;
+    }
+
+};
